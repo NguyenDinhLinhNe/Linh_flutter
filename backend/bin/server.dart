fc81404658acfd764c87d11a507a791b97ae6b26
NguyenDinhLinhNe <dinhlinhne2711@gmail.com>
2024-09-20 12:04:30 +0700

    Điều chỉnh routers và thêm endpoint api/v1/submit vào server

--- backend/bin/server.dart
+++ backend/bin/server.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:shelf/shelf.dart';
@@ -5,14 +6,60 @@ import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
 
 // Configure routes.
-final _router = Router()
+final _router = Router(notFoundHandler: _notFoundHandler)
   ..get('/', _rootHandler)
   ..get('api/v1/check', _checkHandler)
-  ..get('/echo/<message>', _echoHandler)
-  ..post('api/v1/check', _submitHandler)
+  ..get('api/v1/echo/<message>', _echoHandler)
+  ..post('api/v1/submit', _submitHandler);
+
+final _header = {'Content-Type' : 'application/json'};
+
+Response _notFoundHandler(Request req){
+  return Response.notFound('Không tìm thấy đường dẫn "${req.url}" trên server');
+}
 
 Response _rootHandler(Request req) {
   return Response.ok('Hello, World!\n');
+}
+
+Response _checkHandler(Request req){
+  return Response.ok(
+    json.encode({'message' : 'Chào mừng đến với web động' }),
+    headers: _header
+  );
+}
+
+Future<Response> _submitHandler(Request req) async {
+  try{
+    final payload = await req.readAsString();
+
+    final data = json.decode(payload);
+
+    final name = data['name'] as String?;
+
+    if(name != null && name.isNotEmpty){
+      final response = {'message' : 'Xin chào $name'};
+
+      return Response.ok(
+        json.encode(response),
+        headers: _header,
+      );
+    }
+    else{
+      final response = {'message' : 'Server không nhận được tên của bạn !'};
+
+      return Response.badRequest(
+        body : json.encode(response),
+        headers: _header,
+      );
+    }
+  } catch(e){
+    final response =  {'message' :  'Yêu cầu không hợp lệ . Lỗi ${e.toString()}'};
+    return Response.badRequest(
+      body : json.encode(response),
+      headers: _header,
+    );
+  }
 }
 
 Response _echoHandler(Request request) {
